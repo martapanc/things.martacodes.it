1c9b057375c3acb3ef75ab3a8e541a5911fd6807
martapanc <marta.panc@gmail.com>
2024-11-06 20:28:15 +0100

    Remove unused imports

--- src/app/blog/categories/page.tsx
+++ src/app/blog/categories/page.tsx
@@ -1,6 +1,4 @@
 import getAllPosts, { listCategoriesWithCounts } from '@/lib/blog-posts';
-import { allCategories } from '@/types/Post';
-import Link from 'next/link';
 import { BlogLayoutWrapper } from '@/app/blog/blog-layout';
 import { CategoryRow } from '@/components/molecules/CategoryRow';
 
